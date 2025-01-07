12e40402222baba3cfda5263f79be7138fa23b70
bigKeed <138622264+bigKeed@users.noreply.github.com>
2025-01-07 16:47:37 +0100

    Fixed update fails

--- routes/task.route.js
+++ routes/task.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require ('../Middlewares/authMiddleware.js')
 const taskController = require('../controllers/task.controller.js')
 
-router.get('/tasks/edit/:id',authMiddleware, taskController.editTask ); 
+router.get('/edit/:id',authMiddleware, taskController.editTask ); 
 router.get('/', authMiddleware, taskController.getTasks);
 router.post('/create', authMiddleware, taskController.createTasks);
 router.post('/edit/:id',authMiddleware, taskController.updateTask);
