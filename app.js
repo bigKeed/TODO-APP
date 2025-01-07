635bf2921691908b754d0de6fd7f3dc83ba798e9
bigKeed <138622264+bigKeed@users.noreply.github.com>
2025-01-07 16:30:06 +0100

    error handling

--- app.js
+++ app.js
@@ -43,4 +43,15 @@ res.render('index')
 app.use('/', authRoutes);
 app.use('/tasks', taskRoutes); 
 
+// Handle 404 Not Found
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'This Route is Not Found' });
+}); 
+
+//Global Error Handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+
 module.exports = app;
